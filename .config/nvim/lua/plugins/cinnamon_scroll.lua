2af9d457159a363e866d046d40028e50ea2d79ec
DMelisena <demonlordx777@gmail.com>
2024-04-11 04:43:59 +0700

    setting the half page scroll speed for nvim

--- .config/nvim/lua/plugins/cinnamon_scroll.lua
+++ .config/nvim/lua/plugins/cinnamon_scroll.lua
@@ -1,7 +1,9 @@
 return {
   "declancm/cinnamon.nvim",
   config = function()
-    require("cinnamon").setup({})
+    require("cinnamon").setup({
+      default_delay = 2
+    })
   end,
 }
 --TODO: add shift+[ and shift+] as the other key
