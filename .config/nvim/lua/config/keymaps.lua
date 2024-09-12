d51402abe198267dea71abb542cd462bb1522c34
DMelisena <demonlordx777@gmail.com>
2024-09-12 16:59:26 +0700

    Adding recenter to cinnamon scroll vim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+local keymap = vim.keymap
+local cinnamon = require("cinnamon")
+cinnamon.setup()
+keymap.set("n", "<C-U>", function() cinnamon.scroll("<C-U>zz") end)
+keymap.set("n", "<C-D>", function() cinnamon.scroll("<C-D>zz") end)
